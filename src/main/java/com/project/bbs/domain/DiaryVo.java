e0435676d2fdf6ba5d307f8c26aba6acdae4d63f
Kang Yi Seong <54849894+KLS13@users.noreply.github.com>
2020-03-04 23:47:12 +0900

    Diary

---
+++ src/main/java/com/project/bbs/domain/DiaryVo.java
@@ -0,0 +1,27 @@
+package com.project.bbs.domain;
+
+public class DiaryVo {
+
+	private String writer, content;
+
+	public DiaryVo() {
+		super();
+		
+	}
+
+	public String getWriter() {
+		return writer;
+	}
+
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+}
